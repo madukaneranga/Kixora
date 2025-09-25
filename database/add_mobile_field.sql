81c08e81f7a8a99aa0adfd80e3faf09a52bf8fa3
MadukaNernaga <madukaneranga.business@gmail.com>
2025-09-25 15:17:01 +0530

    Commit

--- database/add_mobile_field.sql
+++
@@ -1,4 +0,0 @@
--- Add mobile field to contact_messages and support_requests tables
-
-ALTER TABLE contact_messages ADD COLUMN IF NOT EXISTS mobile TEXT;
-ALTER TABLE support_requests ADD COLUMN IF NOT EXISTS mobile TEXT;
